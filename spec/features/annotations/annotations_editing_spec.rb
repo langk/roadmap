2d428500b4c0bf6eedc89edf0698a2599b350176
Bodacious <gavin@katanacode.com>
2018-10-16 09:41:52 +0100

    Fix tests breaking with seed 60029

--- spec/features/annotations/annotations_editing_spec.rb
+++ spec/features/annotations/annotations_editing_spec.rb
@@ -38,6 +38,8 @@ RSpec.feature "Annotations::Editing", type: :feature do
       click_link "Customise"
     }.to change { Template.count }.by(1)
 
+    # New Template created
+    template = Template.last
     click_link "Customise phase"
 
     click_link section.title
@@ -48,7 +50,7 @@ RSpec.feature "Annotations::Editing", type: :feature do
     end
     question = Question.last
     # NOTE: This is question 2, since Annotation was copied upon clicking "Customise"
-    within("#edit_question_#{question.id + 1}") do
+    within("#edit_question_#{template.question_ids.last}") do
       # Expect it to destroy the newly cleared Annotation
       expect { click_button 'Save' }.not_to change { Annotation.count }
     end
@@ -65,14 +67,15 @@ RSpec.feature "Annotations::Editing", type: :feature do
     expect {
       click_link "Customise"
     }.to change { Template.count }.by(1)
+    template = Template.last
     click_link "Customise phase"
     click_link section.title
     # NOTE: This is annotation 2, since Annotation was copied upon clicking "Customise"
     within("fieldset#fields_annotation_2") do
-      tinymce_fill_in("question_annotations_attributes_annotation_#{Question.last.id + 1}_text", with: " ")
+      tinymce_fill_in("question_annotations_attributes_annotation_#{template.question_ids.last}_text", with: " ")
     end
     # NOTE: This is question 2, since Annotation was copied upon clicking "Customise"
-    within('#edit_question_2') do
+    within("#edit_question_#{template.question_ids.last}") do
       # Expect it to destroy the newly cleared Annotation
       expect { click_button 'Save' }.to change { Annotation.count }.by(-1)
     end
